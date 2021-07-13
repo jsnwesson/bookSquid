c921809d13d127192fd9860a18f141e5d80cfef5
Jason Wesson <jsnwesson@gmail.com>
2021-07-13 00:15:36 -0500

    implement the HOC for reading lists

--- bookclub-frontend/src/components/bookclub/BookGallery.jsx
+++ bookclub-frontend/src/components/bookclub/BookGallery.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import { } from 'react-bootstrap';
+import Slider from 'react-slick';
+import { Card } from 'react-bootstrap';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-const BookGallery = (props) => {
-
+const withSlide = (wrappedComponent, selectData) => {
+  var settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+  };
   return (
-    <div></div>
+    <div>
+      <h2>{selectData.title}</h2>
+      <Slider {...settings}>
+        {selectData.list.map((book) => {
+          return (
+            <div>
+              <Card style={{ width: '10rem' }}>
+                <Card.Img variant='top' src={book.thumbnail} />
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <Card.Text>{book.author}</Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          )
+        })}
+      </Slider>
+    </div>
   );
 }
 
-export default BookGallery;
+export default withSlide;
