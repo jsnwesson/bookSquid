527eaeca9b834075f9232f8bdb088f0f030a554d
Jason Wesson <jsnwesson@gmail.com>
2021-07-13 21:51:55 -0500

    replace setBook hook and authorization setup in backend, as well as add rows/cols to carousel

--- bookclub-frontend/src/components/bookclub/BookGallery.jsx
+++ bookclub-frontend/src/components/bookclub/BookGallery.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Slider from 'react-slick';
-import { Card, } from 'react-bootstrap';
+import { Card, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -43,27 +43,31 @@ const withSlide = (wrappedComponent, selectData, setBook) => {
   };
   return (
     <div>
-      <h2>{selectData.title}</h2>
-      <Slider className='slider' {...settings}>
-        {selectData.list.map((book) => {
-          return (
-            <div>
-              <Card
-                bg='light'
-                text='dark'
-                style={{ width: '10rem' }}>
-                <Link onClick={() => { setBook(book) }} to={`/book/${book.bookId}`}>
-                  <Card.Img variant='top' src={book.thumbnail} />
-                </Link>
-                <Card.Body>
-                  <Card.Title>{book.title}</Card.Title>
-                  <Card.Text>{book.authors[0]}</Card.Text>
-                </Card.Body>
-              </Card>
-            </div>
-          )
-        })}
-      </Slider>
+      <Row>
+        <Col>
+          <h2>{selectData.title}</h2>
+          <Slider className='slider' {...settings}>
+            {selectData.list.map((book) => {
+              return (
+                <div>
+                  <Card
+                    bg='light'
+                    text='dark'
+                    style={{ width: '10rem' }}>
+                    <Link onClick={() => { setBook(book) }} to={`/book/${book.bookId}`}>
+                      <Card.Img variant='top' src={book.thumbnail} />
+                    </Link>
+                    <Card.Body>
+                      <Card.Title>{book.title}</Card.Title>
+                      <Card.Text>{book.authors[0]}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </div>
+              )
+            })}
+          </Slider>
+        </Col>
+      </Row>
     </div>
   );
 }
--- bookclub-frontend/src/pages/profile.jsx
+++ bookclub-frontend/src/pages/profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Container, Row, Col} from 'react-bootstrap';
+import { Button, Container} from 'react-bootstrap';
 import SignOut from '../components/sessions/SignOut';
 import withSlide from '../components/bookclub/BookGallery.jsx';
 import { favorites, previouslyRead, wantToRead } from '../dummyData/booklist.jsx';
@@ -53,21 +53,9 @@ const Profile = (props) => {
         <h3>things that go on the profile page should be here and is only for a user that is logged into an account</h3>
       </>
       <Container fluid='sm'>
-        <Row>
-          <Col>
-            {withSlide(faves, faveList, props.setBook)}
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            {withSlide(prevRead, prevList, props.setBook)}
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            {withSlide(toRead, toReadList, props.setBook)}
-          </Col>
-        </Row>
+        {withSlide(faves, faveList, props.setBook)}
+        {withSlide(prevRead, prevList, props.setBook)}
+        {withSlide(toRead, toReadList, props.setBook)}
       </Container>
     </div>
   )
