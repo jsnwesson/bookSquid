7b809ae96a02b436d7d93f1c427674352d864c18
Jacob Ward <smilinjake@gmail.com>
2021-07-15 14:51:30 -0500

    new and hopefully and improved

--- bookclub-backend/authenticateToken.js
+++ bookclub-backend/authenticateToken.js
@@ -6,13 +6,26 @@ admin.initializeApp({
   // databaseURL: 'https://phone-book-fe436.firebaseio.com',
 });
 
+// async function decodeIDToken(req, res, next) {
+//   //////////////////////////////////////////////
+//   const header = req.headers.authorization;
+//   if (header !== 'Bearer null' && req.headers.authorization.startsWith('Bearer ')) {
+//     //////////////////////////////////////////////
+//     // const header = req.headers?.authorization;
+//     // if (header !== 'Bearer null' && req.headers?.authorization?.startsWith('Bearer ')) {
+//     const idToken = req.headers.authorization.split('Bearer ')[1];
+//     try {
+//       const decodedToken = await admin.auth().verifyIdToken(idToken);
+//       req['currentUser'] = decodedToken;
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }
+//   next();
+// };
 async function decodeIDToken(req, res, next) {
-  //////////////////////////////////////////////
-  const header = req.headers.authorization;
-  if (header !== 'Bearer null' && req.headers.authorization.startsWith('Bearer ')) {
-    //////////////////////////////////////////////
-    // const header = req.headers?.authorization;
-    // if (header !== 'Bearer null' && req.headers?.authorization?.startsWith('Bearer ')) {
+  const header = req.headers?.authorization;
+  if (header !== 'Bearer null' && req.headers?.authorization?.startsWith('Bearer ')) {
     const idToken = req.headers.authorization.split('Bearer ')[1];
     try {
       const decodedToken = await admin.auth().verifyIdToken(idToken);
